702d8b6d3e83a0635570e97770172b4948f491b3
KJH <kjh@KJHs-MacBook-Pro.local>
2023-09-23 00:02:33 +0900

    :feat: feat: mixins 기반 클래스 적용

--- example/urls.py
+++ example/urls.py
@@ -1,15 +1,23 @@
 from django.urls import path, include
-# from .views import HelloAPI, bookAPI, booksAPI
+from .views import HelloAPI, bookAPI, booksAPI
 from .views import BookAPI, BooksAPI
+from .views import BookAPIMixins, BooksAPIMixins
+
 
 urlpatterns = [
-    # path('hello/', HelloAPI),
+    path('hello/', HelloAPI),
 
     # fbv url 연결
-    # path('fbv/books/', booksAPI),
-    # path('fbv/book/<int:bid>/', bookAPI),
+    path('fbv/books/', booksAPI),
+    path('fbv/book/<int:bid>/', bookAPI),
 
     # cbv url 연결    
     path('cbv/books', BooksAPI.as_view()),
     path('cbv/book/<int:bid>/', BookAPI.as_view()),
+
+    # cbv by mixins url 연결
+    path('mixin/books/', BooksAPIMixins.as_view()),
+    path('mixin/book/<int:bid>/', BookAPIMixins.as_view()),
+    
+
 ]
--- example/views.py
+++ example/views.py
@@ -6,42 +6,44 @@ from rest_framework.generics import get_object_or_404 # get_object_or_404 불러
 from .models import Book
 from .serializers import BookSerializer
 
+from rest_framework import generics
+from rest_framework import mixins
 
 # Create your views here.
-# class HelloAPI(APIView):
-#     def get(self, request):
-#         return Response('hello world!')
+class HelloAPI(APIView):
+    def get(self, request):
+        return Response('hello world!')
     
 
 # FBV
-# @api_view(['GET'])
-# def HelloAPI(request):
-#     return Response('hello world!')
+@api_view(['GET'])
+def HelloAPI(request):
+    return Response('hello world!')
 
 
-# @api_view(['GET', 'POST'])
-# def booksAPI(request):
-#     if request.method == 'GET':
-#         books = Book.objects.all()
-#         serializer = BookSerializer(books, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#     elif request.method == 'POST':
-#         serializer = BookSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#     else:
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET', 'POST'])
+def booksAPI(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-# @api_view(['GET'])
-# def bookAPI(request, bid):
-#     book = get_object_or_404(Book, bid=bid)
-#     serializer = BookSerializer(book)
-#     return Response(serializer.data, status=status.HTTP_200_OK)    
+@api_view(['GET'])
+def bookAPI(request, bid):
+    book = get_object_or_404(Book, bid=bid)
+    serializer = BookSerializer(book)
+    return Response(serializer.data, status=status.HTTP_200_OK)    
 
 
-# CBV
+# CBV by APIView
 class BooksAPI(APIView):
     def get(self, request):
         books = Book.objects.all()
@@ -61,5 +63,35 @@ class BookAPI(APIView):
         book = get_object_or_404(Book, bid=bid)
         serializer = BookSerializer(book)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# CBV by mixins
+class BooksAPIMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class BookAPIMixins(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'bid'
+
+    def get(self, request, *args, **kwargs): # 1권 정보 불러오기
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+    
+
+
 
 
